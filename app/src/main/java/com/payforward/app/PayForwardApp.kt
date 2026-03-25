cbfe78f3abc32bd8c1bf29defbd211901f6959da
Silicon-Developer <you@example.com>
2026-03-25 20:51:07 +0530

    Fix PayForwardApp getDatabase reference

--- app/src/main/java/com/payforward/app/PayForwardApp.kt
+++ app/src/main/java/com/payforward/app/PayForwardApp.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 
 class PayForwardApp : Application() {
 
-    val database by lazy { AppDatabase.getInstance(this) }
+    val database by lazy { AppDatabase.getDatabase(this) }
     private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     override fun onCreate() {
